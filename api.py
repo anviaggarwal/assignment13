42af0b7e38a9fe6c213b013c45dfb2956e6267cf
anviaggarwal <42367097+anviaggarwal@users.noreply.github.com>
2018-09-05 10:07:41 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,9 +1,9 @@
 #Q.1-Use the "https://api.forismatic.com/api/1.0/" api to get random quotes using the correct endpoints.
 
 import requests
-info=requests.get("https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en")
-print(info.status_code)
+response=requests.get("https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en")
+print(response.status_code)
 import json
-detail=info.json()
-print(type(detail))
-print(detail["quoteText"])
+data=response.json()
+print(type(data))
+print(data["quoteText"])
